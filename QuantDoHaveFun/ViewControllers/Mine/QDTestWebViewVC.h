db22daca6a8ec413ee7b9d70a2d2ee4f493f9f60
Charles Ran <jin7618@qq.com>
2019-05-06 18:08:28 +0800

    UI改版。

---
+++ QuantDoHaveFun/ViewControllers/Mine/QDTestWebViewVC.h
@@ -0,0 +1,18 @@
+//
+//  QDTestWebViewVC.h
+//  TravelPoints
+//
+//  Created by WJ-Shao on 2019/4/28.
+//  Copyright © 2019 Charles Ran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QDTestWebViewVC : UIViewController
+
+@property (nonatomic, strong) NSString *htmlStr;
+@end
+
+NS_ASSUME_NONNULL_END
