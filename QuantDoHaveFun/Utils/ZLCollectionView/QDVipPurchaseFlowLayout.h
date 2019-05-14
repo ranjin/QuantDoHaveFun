4fc9aea67ff26f5b7ca0957c32c4e34601f7b61b
Charles Ran <jin7618@qq.com>
2019-05-14 09:31:18 +0800

    ZLCollectionView路径信息。

---
+++ QuantDoHaveFun/Utils/ZLCollectionView/QDVipPurchaseFlowLayout.h
@@ -0,0 +1,17 @@
+//
+//  QDVipPurchaseFlowLayout.h
+//  TravelPoints
+//
+//  Created by 冉金 on 2019/2/17.
+//  Copyright © 2019 Charles Ran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QDVipPurchaseFlowLayout : UICollectionViewFlowLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ QuantDoHaveFun/Utils/ZLCollectionView/QDVipPurchaseFlowLayout.m
@@ -0,0 +1,73 @@
+//
+//  QDVipPurchaseFlowLayout.m
+//  TravelPoints
+//
+//  Created by 冉金 on 2019/2/17.
+//  Copyright © 2019 Charles Ran. All rights reserved.
+//
+
+#import "QDVipPurchaseFlowLayout.h"
+#import "ZLFlowLayoutConstants.h"
+
+@implementation QDVipPurchaseFlowLayout
+- (instancetype)init
+{
+    self = [super init];
+    if (self)
+    {
+        self.minimumLineSpacing = 15.0f;
+        self.itemSize = CGSizeMake(SCREEN_WIDTH*0.58,SCREEN_HEIGHT*0.5);
+        self.scrollDirection = UICollectionViewScrollDirectionHorizontal;//设置为水平滑动
+        self.sectionInset = UIEdgeInsetsMake(0, 30, 0, 30);
+    }
+    return self;
+}
+
+- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)oldBounds
+{
+    return YES;
+}
+
+- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
+{    
+    NSArray * array = [super layoutAttributesForElementsInRect:rect];
+    
+    CGRect visiableRect;
+    visiableRect.origin = self.collectionView.contentOffset;
+    visiableRect.size = self.collectionView.bounds.size;
+    
+    for (UICollectionViewLayoutAttributes * attributes in array)
+    {
+        if (CGRectIntersectsRect(attributes.frame, rect))
+        {
+            CGFloat distance = CGRectGetMidX(visiableRect) - attributes.center.x;
+            distance = ABS(distance);
+            if (distance < SCREEN_WIDTH/2 + self.itemSize.width)
+            {
+                CGFloat zoom = 1 + ITEM_ZOOM * (1 - distance/THE_ACTIVE_DISTANCE);
+                attributes.transform3D = CATransform3DMakeScale(zoom, zoom, 1.0f);
+                attributes.transform3D = CATransform3DTranslate(attributes.transform3D, 0, -zoom * 25, 0);
+                attributes.alpha = zoom - ITEM_ZOOM;
+            }
+        }
+    }
+    return array;
+}
+
+- (CGPoint )targetContentOffsetForProposedContentOffset:(CGPoint)proposedContentOffset withScrollingVelocity:(CGPoint)velocity
+{
+    CGFloat offsetAdjustment = MAXFLOAT;
+    CGFloat horizontalCenter_X = proposedContentOffset.x + CGRectGetWidth(self.collectionView.bounds)/2.0;
+    CGRect targetRect = CGRectMake(proposedContentOffset.x, 0, self.collectionView.bounds.size.width, self.collectionView.bounds.size.height);
+    NSArray *array = [super layoutAttributesForElementsInRect:targetRect];
+    for (UICollectionViewLayoutAttributes * attributes in array)
+    {
+        CGFloat itemHorizontalCenter_X = attributes.center.x;
+        if (ABS(itemHorizontalCenter_X - horizontalCenter_X) < ABS(offsetAdjustment))
+        {
+            offsetAdjustment = itemHorizontalCenter_X - horizontalCenter_X;
+        }
+    }
+    return CGPointMake(proposedContentOffset.x + offsetAdjustment, proposedContentOffset.y);
+}
+@end
